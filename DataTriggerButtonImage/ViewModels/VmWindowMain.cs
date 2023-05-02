6cc622070a668bab49f3c4ef890c2ddb08e25fb3
Hannes Hold <hannes.hold@outlook.com>
2023-05-02 19:53:53 +0200

    Add project files.

---
+++ DataTriggerButtonImage/ViewModels/VmWindowMain.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace DataTriggerButtonImage.ViewModels
+{
+    public partial class VmWindowMain : ObservableObject
+    {
+        #region Properties
+
+        [ObservableProperty]
+        private bool _isPlaying = false;
+
+        #endregion
+
+        #region Commands
+
+        [RelayCommand(CanExecute = nameof(CanExecutePlay))]
+        private void Play()
+        {
+            IsPlaying = !IsPlaying;
+        }
+
+        private bool CanExecutePlay()
+        {
+            return true;
+        }
+
+        #endregion
+
+    }
+}
